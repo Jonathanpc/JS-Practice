82b62467881e7080e1da7b30e83b4b47f56f5e13
Joni Perez <joniperez@Jonis-MacBook-Pro.local>
2025-02-05 16:34:13 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,88 @@
+// // clg --> console.log("yuhh");
+
+// //  (reserved keyword) (variable name) (type of value)
+
+// // declare variable:    let and var variables are able to be changed
+// // let name;
+
+// // assign value:
+// let banana;
+// banana = "banana";
+
+// // Change value for the let variable:
+// banana = "apple";
+
+// console.log(banana);
+
+// // const variable
+// const youtubeChannel = "Mrbeast";
+// // youtubeChannel = "JonYT";
+// // Will give error because variable is a const (cannot be changed)
+// console.log(youtubeChannel);
+
+// let name = "Jonathan";
+// let whatDoYouWannaBecomeInYourLife = "Programmer";
+// let gender = "male";
+// let twitterHandle = "JoniTwitter";
+// console.log(name);
+// console.log(whatDoYouWannaBecomeInYourLife);
+// console.log(gender);
+// console.log(twitterHandle);
+
+// Primitive Types
+
+//Excersice 2
+// let firstFavNum = 15;
+// let secondFavNum = 7;
+// console.log(firstFavNum + secondFavNum);
+// console.log(firstFavNum - secondFavNum);
+// console.log(firstFavNum * secondFavNum);
+// console.log(firstFavNum / secondFavNum);
+// console.log(firstFavNum % secondFavNum);
+// console.log(firstFavNum ** secondFavNum);
+
+// // Excerise 3 Boolean
+// let isJsProgramingLanguage = true;
+// let isJsHard = false;
+// let favNum = 15;
+// console.log(favNum + undefined);
+
+// comparison operators
+// === checks type and value and vice versa for !==
+// == would only check the value vice versa for !=
+
+// Excersize 4 comparison
+// let firstFavNum = 15;
+// let secondFavNum = 7;
+// console.log(firstFavNum > secondFavNum);
+// console.log(firstFavNum < secondFavNum);
+// console.log(firstFavNum >= secondFavNum);
+// console.log(firstFavNum <= secondFavNum);
+// console.log(firstFavNum === secondFavNum);
+// console.log(firstFavNum == secondFavNum);
+// console.log(firstFavNum !== secondFavNum);
+// console.log(firstFavNum != secondFavNum);
+
+// Strings
+// let firstName = "Jonathan";
+// let secondName = "Prz";
+
+// 1.Concatenation
+// let firstName = "Jonathan "; //Needed space because it wont put the space between them
+// let secondName = "Prz";
+// let fullName = firstName.concat(secondName);
+// console.log(fullName);
+
+// 2.Append
+// let firstName = "Jonathan";
+// firstName += " Castro"; //Space added because space isnt added again
+// console.log(firstName);
+
+// 3.Length
+// console.log(firstName.length);
+
+// 4.Cases
+// firstName.toLowerCase()
+// firstName.toUpperCase();
+
+// 5.Slice
